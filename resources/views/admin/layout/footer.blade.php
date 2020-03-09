64d1d6f567454550999cd95e779c5ade4fa28240
ngocbao2510 <nbngoc.17it2@sict.udn.vn>
2020-03-09 23:45:45 +0700

    done date 9/3

---
+++ resources/views/admin/layout/footer.blade.php
@@ -0,0 +1,93 @@
+</div>
+
+<div id="styleSelector">
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<!-- Warning Section Starts -->
+<!-- Older IE warning message -->
+<!--[if lt IE 10]>
+<div class="ie-warning">
+<h1>Warning!!</h1>
+<p>You are using an outdated version of Internet Explorer, please upgrade <br/>to any of the following web browsers to access this website.</p>
+<div class="iew-container">
+<ul class="iew-download">
+<li>
+<a href="http://www.google.com/chrome/">
+<img src="../files/assets/images/browser/chrome.png" alt="Chrome">
+<div>Chrome</div>
+</a>
+</li>
+<li>
+<a href="https://www.mozilla.org/en-US/firefox/new/">
+<img src="../files/assets/images/browser/firefox.png" alt="Firefox">
+<div>Firefox</div>
+</a>
+</li>
+<li>
+<a href="http://www.opera.com">
+<img src="../files/assets/images/browser/opera.png" alt="Opera">
+<div>Opera</div>
+</a>
+</li>
+<li>
+<a href="https://www.apple.com/safari/">
+<img src="../files/assets/images/browser/safari.png" alt="Safari">
+<div>Safari</div>
+</a>
+</li>
+<li>
+<a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie">
+<img src="../files/assets/images/browser/ie.png" alt="">
+<div>IE (9 & above)</div>
+</a>
+</li>
+</ul>
+</div>
+<p>Sorry for the inconvenience!</p>
+</div>
+<![endif]-->
+<!-- Warning Section Ends -->
+<!-- Required Jquery -->
+<script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script type="text/javascript" src="../files/bower_components/jquery/js/jquery.min.js"></script>
+<script type="text/javascript" src="../files/bower_components/jquery-ui/js/jquery-ui.min.js"></script>
+<script type="text/javascript" src="../files/bower_components/popper.js/js/popper.min.js"></script>
+<script type="text/javascript" src="../files/bower_components/bootstrap/js/bootstrap.min.js"></script>
+<!-- jquery slimscroll js -->
+<script type="text/javascript" src="../files/bower_components/jquery-slimscroll/js/jquery.slimscroll.js"></script>
+<!-- modernizr js -->
+<script type="text/javascript" src="../files/bower_components/modernizr/js/modernizr.js"></script>
+<!-- Chart js -->
+<script type="text/javascript" src="../files/bower_components/chart.js/js/Chart.js"></script>
+<!-- amchart js -->
+<script src="../files/assets/pages/widget/amchart/amcharts.js"></script>
+<script src="../files/assets/pages/widget/amchart/serial.js"></script>
+<script src="../files/assets/pages/widget/amchart/light.js"></script>
+<script src="../files/assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script type="text/javascript" src="../files/assets/js/SmoothScroll.js"></script>
+<script src="../files/assets/js/pcoded.min.js"></script>
+<!-- custom js -->
+<script src="../files/assets/js/vartical-layout.min.js"></script>
+<script type="text/javascript" src="../files/assets/pages/dashboard/custom-dashboard.js"></script>
+<script type="text/javascript" src="../files/assets/js/script.min.js"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-23581568-13');
+</script>
+</body>
+
+
+<!-- Mirrored from colorlib.com/polygon/adminty/default/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 29 Nov 2018 03:56:08 GMT -->
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::group(['prefix' => 'admin'], function () {
-    Route::get('/', function () {
-        return view('welcome');
-    });
-    Route::get('/hello/{year}/{yourname?}', function($year, $yourname = null){
-        $hello_string = '';
-        if($yourname == null){
-            $hello_string = 'Hello world, ' . $year;
-        }else{
-            $hello_string = 'Hello world, ' . $year . '. My name is ' . $yourname;
-        }
-        return view('index')->with('hello_str', $hello_string);
-    }); 
+    Route::get('/', 'AdminController@index')->name('admin');
+     
 });
